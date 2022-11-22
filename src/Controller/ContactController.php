3c9cf95c8c2763c98c19b19dbf2cde79101ee30b
Jules Bousrez <38622433+julesbsz@users.noreply.github.com>
2022-11-22 14:11:44 +0100

    New Packages
    NPM
    Navbar
    Events & Contact pages

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController extends AbstractController
+class ContactController extends AbstractController
 {
-    #[Route('/', name: 'accueil')]
+    #[Route('/contact', name: 'contact')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
         ]);
     }
 }
--- src/Controller/EvenementsController.php
+++ src/Controller/EvenementsController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController extends AbstractController
+class EvenementsController extends AbstractController
 {
-    #[Route('/', name: 'accueil')]
+    #[Route('/evenements', name: 'evenements')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+        return $this->render('evenements/index.html.twig', [
+            'controller_name' => 'EvenementsController',
         ]);
     }
 }
