f53a2d70aa21506d9119c88cbfa6f88a00834136
Jules Bousrez <38622433+julesbsz@users.noreply.github.com>
2022-11-27 23:40:19 +0100

    Adding NavBar

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 }
 
 Encore
+    // Importing Images
+    .copyFiles({
+        from: './assets/images',
+    })
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
