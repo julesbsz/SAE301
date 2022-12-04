303560a50664bcc96158f418be611d3ad96c3822
Jules Bousrez <38622433+julesbsz@users.noreply.github.com>
2022-12-04 14:58:08 +0100

    Add order in db

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Orders;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +15,8 @@ class PaymentController extends AbstractController
 {
     #[Route('/paiement/{status}', name: 'app_payment', requirements: ['status' => 'success|cancel'], defaults: ['status' => ''])]
     public function index(
-        Request $request
+        Request $request,
+        EntityManagerInterface $entityManager,
     ): Response
     {
         if($this->getUser() === null) {
@@ -29,8 +33,24 @@ class PaymentController extends AbstractController
             $form->handleRequest($request);
             if ($form->isSubmitted() && $form->isValid()) {
                 $data = $form->getData();
-                // generate pdf and send email
-                return $this->redirectToRoute('app_payment', ['status' => "success"]);
+                $order = new Orders();
+                $order->setDate(new \DateTime());
+                $order->setFirstname($data['firstname']);
+                $order->setLastname($data['lastname']);
+                $order->setAddress($data['address']);
+                $order->setCity($data['city']);
+                $order->setZipcode($data['zip']);
+                $order->setCountry($data['country']);
+                $order->setPhone($data['phone']);
+                $order->setUser($this->getUser());
+
+                try {
+                    $entityManager->persist($order);
+                    $entityManager->flush();
+                    return $this->redirectToRoute('app_payment', ['status' => "success"]);
+                } catch (ORMException $e) {
+                    return $this->redirectToRoute('app_payment', ['error' => 'Une erreur est survenue lors de la création de la commande']);
+                }
             }
 
             return $this->renderForm('payment/index.html.twig', [
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -29,6 +31,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Orders::class)]
+    private Collection $orders;
+
+    public function __construct()
+    {
+        $this->orders = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -98,5 +108,35 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    /**
+     * @return Collection<int, Orders>
+     */
+    public function getOrders(): Collection
+    {
+        return $this->orders;
+    }
+
+    public function addOrder(Orders $order): self
+    {
+        if (!$this->orders->contains($order)) {
+            $this->orders->add($order);
+            $order->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrder(Orders $order): self
+    {
+        if ($this->orders->removeElement($order)) {
+            // set the owning side to null (unless already changed)
+            if ($order->getUser() === $this) {
+                $order->setUser(null);
+            }
+        }
+
+        return $this;
     }
 }
