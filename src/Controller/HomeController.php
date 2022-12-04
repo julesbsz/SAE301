83c396ac77bf437b3a9bf347aabc49d42934745c
Joron Paul <paul.joron@orange.fr>
2022-12-04 12:47:50 +0100

    Home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,41 @@
 
 namespace App\Controller;
 
+use App\Repository\EventsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home', requirements:['id' => Requirement::ASCII_SLUG,], defaults:['id'=>'tous'])]
+  public function index(
+        $id,
+        EventsRepository $eventsRepository,
+    ): Response
     {
-        return $this->render('home/index.html.twig', []);
+        if($id === 'tous') {
+            $events = $eventsRepository->findAll();
+
+            if($events === null) {
+                throw $this->createNotFoundException('Aucun événement trouvé');
+            } else {
+                return $this->render('home/index.html.twig', [
+                    'events' => $events,
+                ]);
+            }
+        } else {
+            $event = $eventsRepository->find($id);
+            if($event === null) {
+                throw $this->createNotFoundException('Aucun événement trouvé');
+            } else {
+                $place = $event->getPlaces();
+                return $this->render('home/index.html.twig', [
+                    'event' => $event,
+                    'place' => $place,
+                ]);
+            }
+        }
     }
 }
--- src/Controller/LieuxController.php
+++ src/Controller/LieuxController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Repository\PlacesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 class LieuxController extends AbstractController
 {
-    #[Route('/lieux', name: 'app_lieux')]
-    public function index(): Response
+    #[Route('/lieux/{id}', name: 'app_lieux', requirements:['id' => Requirement::ASCII_SLUG,], defaults:['id'=>'tous'])]
+    public function index( $id, PlacesRepository $placesRepository,): Response
     {
-        return $this->render('lieux/lieux.html.twig', [
-            'controller_name' => 'LieuxController',
-        ]);
+        if($id === 'tous') {
+            $places = $placesRepository->findAll();
+
+            if($places === null) {
+                throw $this->createNotFoundException('Aucun lieu trouvé');
+            } else {
+                return $this->render('lieux/lieux.html.twig', [
+                    'places' => $places,
+                ]);
+            }
+        } else {
+            $place = $placesRepository->find($id);
+            if($place === null) {
+                throw $this->createNotFoundException('Aucun lieu trouvé');
+            } else {
+                $events = $place->getEvents();
+                return $this->render('lieux/lieux.html.twig', [
+                    'place' => $place,
+                    'events' => $events,
+                ]);
+            }
+        }
     }
 }
